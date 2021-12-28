36cb154d1071bf91daeaf7ff22466d0d052082b6
wh-99 <rgwh0404@gmail.com>
2021-12-28 09:42:28 +0900

    my_typeTrait

--- typeTrait/typeTrait.h
+++ typeTrait/typeTrait.h
@@ -1,12 +1,12 @@
 #ifndef __TYPE_TRAIT_H__
 #define __TYPE_TRAIT_H__
 
-namespace TraitBase {
-    template <typename T> constexpr bool is_const          = false;
-    template <typename T> constexpr bool is_const<T const> = true;
+namespace {
+    template <typename T> constexpr bool is_const_base          = false;
+    template <typename T> constexpr bool is_const_base<T const> = true;
 
-    template <typename T> constexpr bool is_pointer     = false;
-    template <typename T> constexpr bool is_pointer<T*> = true;
+    template <typename T> constexpr bool is_pointer_base     = false;
+    template <typename T> constexpr bool is_pointer_base<T*> = true;
 }
 
 template <typename T> struct remove_reference      { using type = T; };
@@ -22,8 +22,8 @@ template <typename T> constexpr bool is_lValueReference<T&>  = true;
 template <typename T> constexpr bool is_rValueReference<T&&> = true;
 
 template <typename T> constexpr bool is_reference = is_lValueReference<T> || is_rValueReference<T>;
-template <typename T> constexpr bool is_pointer   = TraitBase::is_pointer<typename remove_const<T>::type>;
-template <typename T> constexpr bool is_const     = TraitBase::is_const<typename remove_reference<T>::type>;
+template <typename T> constexpr bool is_pointer   = is_pointer_base<typename remove_const<T>::type>;
+template <typename T> constexpr bool is_const     = is_const_base<typename remove_reference<T>::type>;
 
 template <typename T, unsigned long long N = 0> constexpr bool is_array       = false;
 template <typename T, unsigned long long N>     constexpr bool is_array<T[N]> = true;
